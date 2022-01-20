00ff6d9a6cf35e20e20dadc5ece09ebcf3813838
Luca <luca.valente@unibo.it>
2022-01-20 12:32:16 +0100

    Correct freq and FDT_ADDR

--- platform/fpga/alsaqr/config.mk
+++ platform/fpga/alsaqr/config.mk
@@ -16,25 +16,12 @@ FW_DYNAMIC = n
 
 FW_JUMP=n
 
-ifeq ($(PLATFORM_RISCV_XLEN), 32)
- # This needs to be 4MB aligned for 32-bit support
- FW_JUMP_ADDR=0x80400000
- else
- # This needs to be 2MB aligned for 64-bit support
- FW_JUMP_ADDR=0x80200000
- endif
-FW_JUMP_FDT_ADDR=0x82200000
 
 # Firmware with payload configuration.
 FW_PAYLOAD=y
-
-ifeq ($(PLATFORM_RISCV_XLEN), 32)
-# This needs to be 4MB aligned for 32-bit support
-  FW_PAYLOAD_OFFSET=0x400000
-else
 # This needs to be 2MB aligned for 64-bit support
-  FW_PAYLOAD_OFFSET=0x200000
-endif
-FW_PAYLOAD_FDT_ADDR=0x82200000
+FW_PAYLOAD_OFFSET=0x200000
+# Let's put the dtb in the L2 spm for now
+FW_PAYLOAD_FDT_ADDR=0x1C000000
 FW_PAYLOAD_ALIGN=0x1000
 
